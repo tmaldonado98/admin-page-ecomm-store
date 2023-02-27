d3fd9a96c4d1f79dc942479910a07cbd5ecae99d
Tomas Maldonado <tmaldonadotrs@gmail.com>
2023-02-26 22:50:21 -0800

    Important advances: Now sending to firebase storage image file from image input field with the name corresponding to 'prodkey' input field value. Now working on enabling/disabling submit button conditionally.

--- admin-page-ecomm-store/src/InventoryCRUD/InsertField.js
+++ admin-page-ecomm-store/src/InventoryCRUD/InsertField.js
@@ -1,6 +1,10 @@
 import { Button } from '@mui/material';
 import './InsertField.css';
 import Axios from 'axios';
+import { initializeApp } from 'firebase/app';
+// import firebase from 'firebase/app';
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { useState } from 'react';
 
 function onlyNumbers(e){
     if (e.keyCode >= 65 && e.keyCode <= 90) {
@@ -9,8 +13,30 @@ function onlyNumbers(e){
     // return true;
 }
 
+const firebaseConfig = {
+   // your Firebase app configuration object
+   apiKey: "AIzaSyAg98s9T7TSlWTywldvgiFqUGlMZlcwy-U",
+   authDomain: "vea-collections.firebaseapp.com",
+   projectId: "vea-collections",
+   storageBucket: "vea-collections.appspot.com",
+   messagingSenderId: "616842123391",
+   appId: "1:616842123391:web:60cbf95825eb5554b6074a",
+   measurementId: "G-S37FFT9HJ3"
+ };
+const firebaseApp = initializeApp(firebaseConfig);
+
+
+// // Create a reference to 'mountains.jpg'
+// const mountainsRef = ref(storage, 'mountains.jpg');
+
+// // Create a reference to 'images/mountains.jpg'
+// const mountainImagesRef = ref(storage, 'images/mountains.jpg');
+
+
 export default function InsertField() {
-    
+
+      const [keyState, setKeyState] = useState('');
+
     function insert(e){
         e.preventDefault();
         let inputValues = {};
@@ -39,7 +65,51 @@ export default function InsertField() {
         // Axios.post('http://localhost:3003/img', inputValues)
         // .catch(error => alert(error))
         // // return input.value = '';
+    
+            // path to storage bucket
+        // gs://vea-collections.appspot.com
+    
     }
+
+    function handleKeyInput (e){
+        console.log(e.target.value);
+        return setKeyState(e.target.value);
+    }
+    
+    // const [status, setStatus] = useState(true)
+
+    function handleFileUpload(event){
+
+
+        // let placeholdername = ;
+        const imgName =  keyState;
+        const file = event.target.files[0];
+
+        const storage = getStorage(firebaseApp);
+        const storageRef = ref(storage, `images/${imgName}.jpg`);
+
+
+        // Create a reference to the file in Firebase Storage
+        // const storageRef = storage.ref().child(`images/${file.name}`);
+
+        uploadBytes(storageRef, file)
+        .then((snapshot) => {
+          
+          console.log('File uploaded successfully with name: ' + imgName);
+        })
+        .catch((error) => {
+          console.error('Error uploading file', error);
+        });
+
+    
+    }
+
+    const formInputs = document.getElementsByTagName('input');
+    // if (for) {
+        
+        // }
+        
+        console.log(formInputs);
 
     return (
         <>
@@ -50,28 +120,28 @@ export default function InsertField() {
                 {/* <div > */}
                     
                     <label for="name">Item Name (no "" double nor '' single quotes)</label>
-                    <input required placeholder='Item Name' name='name' type="text"/>
+                    <input required={true} placeholder='Item Name' name='name' type="text"/>
                     
                     <label for="size">Size (format as: number" x number")</label>
-                    <input required placeholder='ex: 30" x 30"' name='size' type="text"/>
+                    <input required={true} placeholder='ex: 30" x 30"' name='size' type="text"/>
                     
                     <label for="medium">Medium</label>
-                    <input required name='medium' type="text"/>
+                    <input required={true} name='medium' type="text"/>
                     
                     <label for="price">Price (numbers only)</label>
-                    <input required placeholder='ex: 20' name='price' inputMode='numeric' pattern="\d*" onKeyDown={onlyNumbers} />
+                    <input required={true} placeholder='ex: 20' name='price' inputMode='numeric' pattern="\d*" onKeyDown={onlyNumbers} />
                     
                     <label for="img">Image File</label>
-                    <input name='img' type="file" multiple="false" accept="image/*" />
+                    <input required={true}  onChange={handleFileUpload} name='img' type="file" multiple="false" accept="image/*" />
                     {/* ADD REQUIRED FOR PHOTO */}
                     {/* <div id='img-preview'><p>Fetch image for preview</p></div> */}
                     
                     <label for="prodkey">Write a product key in all lowercase or all uppercase. <br/> Recommended to assign one alphabetic integer per inventory item.<br/> For example: "one" for item 1, "two" for item 2, "three" for item 3, etc. </label>
-                    <input required name='prodkey' type="text"/>
+                    <input required={true} onKeyUp={handleKeyInput} name='prodkey' type="text"/>
                     <br/>
                     {/* <label for='submit'>Add Product To Inventory And Present On Live Site</label> */}
-                    <Button type='submit' variant='contained' onClick={insert}>Add Product</Button>
-                   
+                    <Button  type='submit' variant='contained' onClick={insert}>Add Product</Button>
+                   {/* disabled={status} */}
             {/* </div> */}
             </form> 
             
@@ -80,3 +150,6 @@ export default function InsertField() {
         </>
     )
 }
+
+
+// export { firebaseConfig, storage } from './firebase'
